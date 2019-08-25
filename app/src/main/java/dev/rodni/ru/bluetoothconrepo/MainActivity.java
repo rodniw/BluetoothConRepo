aa3bcff0a5927fa92ba421ab2d45d8ae4ed85639
Rodion Pushkin <47295698+rodniw@users.noreply.github.com>
2019-08-25 20:31:48 +0300

    broadcast receiver and bluetooth adapter.

--- app/src/main/java/dev/rodni/ru/bluetoothconrepo/MainActivity.java
+++ app/src/main/java/dev/rodni/ru/bluetoothconrepo/MainActivity.java
@@ -3,17 +3,53 @@ package dev.rodni.ru.bluetoothconrepo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.TextView;
+
+import static android.bluetooth.BluetoothAdapter.STATE_OFF;
+import static android.bluetooth.BluetoothAdapter.STATE_ON;
+import static android.bluetooth.BluetoothAdapter.STATE_TURNING_OFF;
+import static android.bluetooth.BluetoothAdapter.STATE_TURNING_ON;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     private BluetoothAdapter bluetoothAdapter;
-    private TextView startButton, stopButton;
+    private Button startButton, stopButton;
+
+    //creating a broadcast broadcastReceiver
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Log.d(TAG, "onReceive: " + action);
+            // when discovery finds a device
+            if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+                Log.d(TAG, "onReceive: action equals ACTION_STATE_CHANGED");
+                switch (state) {
+                    case STATE_OFF:
+                        Log.d(TAG, "onReceive: STATE_OFF");
+                        break;
+                    case STATE_TURNING_OFF:
+                        Log.d(TAG, "onReceive: STATE_TURNING_OFF");
+                        break;
+                    case STATE_ON:
+                        Log.d(TAG, "onReceive: STATE_ON");
+                        break;
+                    case STATE_TURNING_ON:
+                        Log.d(TAG, "onReceive: STATE_TURNING_ON");
+                        break;
+                }
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,16 +61,14 @@ public class MainActivity extends AppCompatActivity {
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        startButton.setOnClickListener(view -> {
-            enableBluetooth();
-        });
+        startButton.setOnClickListener(view -> enableBluetooth());
 
-        stopButton.setOnClickListener(view -> {
-            disableBluetooth();
-        });
+        stopButton.setOnClickListener(view -> disableBluetooth());
     }
 
+    //method which switch on the bluetooth
     private void enableBluetooth() {
+        Log.d(TAG, "enableBluetooth: called");
         if (bluetoothAdapter == null) {
             Log.d(TAG, "enableBluetooth: Does not have capabilities.");
         }
@@ -46,7 +80,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //method which switch off the bluetooth
     private void disableBluetooth() {
+        Log.d(TAG, "disableBluetooth: called");
         if (bluetoothAdapter.isEnabled()) {
             bluetoothAdapter.disable();
 
@@ -54,8 +90,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //method that send broadcast about bluetooth connectivity
     private void createAndRegBluetoothIntent() {
+        Log.d(TAG, "createAndRegBluetoothIntent: called");
         IntentFilter bluetoothIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
-        registerReceiver(BTBroadcastReceiver, bluetoothIntent);
+        registerReceiver(broadcastReceiver, bluetoothIntent);
+    }
+
+    //unregister my receiver inside on destroy
+    //maybe its better to do it inside onStop ------ FIND INFO ABOUT IT
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy: called");
+        super.onDestroy();
+        unregisterReceiver(broadcastReceiver);
     }
 }
