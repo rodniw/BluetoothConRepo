4cdaa5121699a700315134b43bc4dc5b4789fda5
rodniw <rodion.prosvirnin@bk.ru>
2019-08-27 11:42:07 +0300

    Created accept thread. This thread runs while listening for incoming connections. It is like a server-side client. It runs until a connection is accepted (or until cancelled).

--- sendingdatachat/src/main/java/dev/rodni/sendingdatachat/BluetoothConnectionService.java
+++ sendingdatachat/src/main/java/dev/rodni/sendingdatachat/BluetoothConnectionService.java
@@ -1,5 +1,88 @@
 package dev.rodni.sendingdatachat;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.UUID;
+
 public class BluetoothConnectionService {
     private static final String TAG = "BluetoothConnectionServ";
+
+    private static final String appName = "MYAPP";
+
+    //took this from google samples
+    private static final UUID MY_UUID_INSECURE =
+            UUID.fromString("8ce255c0-200a-11e0-ac64-0800200c9a66");
+
+    private AcceptThread insecureAcceptThread;
+
+    //set this properties through the constructor
+    private final BluetoothAdapter bluetoothAdapter;
+    private Context context;
+
+    public BluetoothConnectionService(Context context) {
+        this.bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        this.context = context;
+    }
+
+    /**
+     * This thread runs while listening for incoming connections. It behaves
+     * like a server-side client. It runs until a connection is accepted
+     * (or until cancelled).
+     */
+    private class AcceptThread extends Thread {
+        //the local server socket
+        private final BluetoothServerSocket bluetoothServerSocket;
+
+        public AcceptThread() {
+            BluetoothServerSocket tmp = null;
+
+            try {
+                //create a new listening server socket
+                tmp = bluetoothAdapter.listenUsingInsecureRfcommWithServiceRecord(appName, MY_UUID_INSECURE);
+                Log.d(TAG, "AcceptThread: setting up a server socket with " + MY_UUID_INSECURE);
+            } catch (IOException e) {
+                Log.d(TAG, "AcceptThread: " + e.getMessage());
+            }
+            bluetoothServerSocket = tmp;
+        }
+
+        public void run() {
+            Log.d(TAG, "run: AcceptThread running.");
+
+            BluetoothSocket bluetoothSocket = null;
+
+            try{
+                // This is a blocking call and will only return on a
+                // successful connection or an exception
+                Log.d(TAG, "run: RFCOM server socket start.....");
+
+                //wait until a connection is made
+                bluetoothSocket = bluetoothServerSocket.accept();
+                //if a connection is successful then i will see this log output
+                Log.d(TAG, "run: RFCOM server socket accepted connection.");
+            }catch (IOException e){
+                Log.e(TAG, "AcceptThread: IOException: " + e.getMessage() );
+            }
+
+            //checking for bluetoothSocket nullability
+            //if (bluetoothSocket != null) {
+            //    connected(bluetoothSocket, device);
+            //}
+        }
+
+        //this cancel method is simply close server socket
+        public void cancel() {
+            Log.d(TAG, "cancel: Canceling AcceptThread.");
+            try {
+                bluetoothServerSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "cancel: Close of AcceptThread ServerSocket failed. " + e.getMessage() );
+            }
+        }
+    }
 }
